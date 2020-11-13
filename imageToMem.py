1ea1ca6d81aec5d93639e44a9cae28470dffc9a6
Samantha Whitt <smw81@duke.edu>
2020-11-13 00:07:39 -0500

    cactus sizes

--- imageToMem.py
+++ imageToMem.py
@@ -4,23 +4,23 @@ from PIL import Image
 def decode():
   image = Image.open(sys.argv[1])
 
-  size = 60
-  image = image.resize((size, size))
-  # image = image.resize((108, 31)) # width, height
+  # size = 60
+  # image = image.resize((size, size))
+  image = image.resize((49, 80)) # width, height
   pixels = image.load()
-  res = [0] * size
-  # res = [0] * 31
-  for row in range(size):
-  # for row in range(31):
-    res[row] = [0] * size
-    # res[row] = [0] * 108
+  # res = [0] * size
+  res = [0] * 80
+  # for row in range(size):
+  for row in range(80):
+    # res[row] = [0] * size
+    res[row] = [0] * 49
 
   for x in range(image.height):
     for y in range(image.width):
       # r,g,b = pixels[y,x]
       r,g,b,a = pixels[y, x]
       # print(r,g,b,a, " ")
-      val = 1 if r < 100 and r > 0 else 0
+      val = 1 if r < 100 and r >= 0 else 0
       res[x][y] = str(val)
 
   return res
